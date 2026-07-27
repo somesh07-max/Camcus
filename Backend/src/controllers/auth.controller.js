2839c1503fc791ab48e2d3cadd1c96614414140f
somesh dewangan <someshdew30062020@gmail.com>
2026-07-27 11:34:55 +0530

    auth cmiddleware added in auth controller

--- Backend/src/controllers/auth.controller.js
+++ Backend/src/controllers/auth.controller.js
@@ -88,6 +88,73 @@ res.status(201).json(({
 
 }
 
+
+async function login(req,res){
+const { email, password } = req.body;
+
+const tempUser = await User.findOne({ email });
+    if(!tempUser){
+     return   res.status(404).json({
+            success:false,
+            message:"User do not exist"
+        })
+    }
+
+const hashedPassword = crypto
+.createHash("sha256")
+.update(password)
+.digest("hex");
+
+if(hashedPassword!=tempUser.password){
+ return   res.status(401).json({
+        success:false,
+        message:"wrong password"
+    })
+}
+
+
+const accessToken = jwt.sign({
+    id:tempUser._id
+},config.JWT_SECRET,{
+    expiresIn:"15m",
+})
+
+const refreshToken = jwt.sign({
+    id:tempUser._id
+},config.JWT_SECRET,{
+    expiresIn:"7d"
+})
+
+res.cookie("refreshToken",refreshToken,{
+    httpOnly:true,
+    secure:process.env.NODE_ENV==="production",
+    sameSite:"strict",
+    maxAge:7*24*60*60*1000
+    
+})
+
+const user = {
+    id: tempUser._id,
+    username: tempUser.username,
+};
+
+res.status(200).json({
+    success:true,
+    message:"User get authorized",
+    accessToken,
+    user,
+
+})
+
+    
+}
+
+
+
+
+
+
+
 async function refreshToken(req,res){
     const refreshToken = req.cookies.refreshToken;
 
@@ -104,7 +171,7 @@ async function refreshToken(req,res){
         );
 
         if(!decoded){
-        return    res.status(404).json({
+        return  res.status(404).json({
                 success:false,
                 message:"User not authorized",
             })
@@ -112,9 +179,9 @@ async function refreshToken(req,res){
 
         const accessToken = jwt.sign(
         {
-            id:decoded._id,
+            id:decoded.id,
         },
-        confi.JWT_SECRET,
+        config.JWT_SECRET,
         {
             expiresIn:"15m"
         });
@@ -148,8 +215,36 @@ async function refreshToken(req,res){
 }
 
 
+async function authenticate(req,res,next){
+        const token = req.headers.authorization?.split(" ")[1];
+
+        if (!token) {
+            return res.status(401).json({
+                success: false,
+                message: "Access token missing"
+            });
+        }
+
+        const decoded = jwt.verify(token, config.JWT_SECRET);
+
+        const user = await User.findById(decoded.id).select("-password");
+
+    if(!user){
+     return   res.status(404).json({
+            success:false,
+            message:"user not found",
+        })
+    }
+
+    req.user = user;
+    next();
+}
+
+
 
 module.exports = {
     Register,
+    login,
     refreshToken,
+    authenticate,
 };
